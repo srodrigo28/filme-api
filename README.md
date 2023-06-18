a5727bef694ca41cd23a77cf89f3c400808a8069
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-06-18 10:48:44 -0300

    create-all-requisition-start

--- README.md
+++ README.md
@@ -5,6 +5,14 @@
 https://www.udemy.com/course/dev-fullstack/learn/lecture/31683080#overview
 ```
 
+### Dados API
+* API Key
+d30221b61194d23fbc7c1d51d99d35c4
+
+* Token de Leitura
+
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzAyMjFiNjExOTRkMjNmYmM3YzFkNTFkOTlkMzVjNCIsInN1YiI6IjY0OGVmZGMwMmY4ZDA5MDEzYWZhNjkxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lffHqbanZmDavdl2BGx_KOhBfyq4AFQ7dK7SIRgsKbw
+
 
 * Link API themoviedb.org
 ```
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
+
+// URL DA API: /movie/now_playing?api_key=d30221b61194d23fbc7c1d51d99d35c4&language=pt-BR
+
 export function Home() {
+    const [filmes, setFilmes] = useState([])
+
+    async function loadFilmes() {
+        const response = await api.get("movie/now_playing", {
+            params: {
+                api_key: "d30221b61194d23fbc7c1d51d99d35c4",
+                language: "pt-BR",
+                page: 1,
+            }
+        })
+        // console.log(response.data)
+        console.log(response.data.results)
+    }
+
+    useEffect(() => {
+        loadFilmes()
+    }, [])
+
     return (
         <div>
             <h1>Bem vindo a Home</h1>
