a1cfba20985cc2cb184a9764f7ae1f9ca1ef11c9
Sebastiao <rodrigoexer1@gmail.com>
2023-06-20 00:44:59 -0300

    create-function-salvar-local-storage

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -8,6 +8,22 @@ export function Filme() {
     const navigate = useNavigate()
     const [filme, setFilme] = useState(true)
     const [loading, setLoading] = useState(true)
+    
+    function salvarFilme(){
+       const minhaLista = localStorage.getItem("@primeflix")
+       let filmesSalvos = JSON.parse(minhaLista) || []
+
+       const hasFilme = filmesSalvos.some( 
+        (filmeSalvo) => filmeSalvo.id === filme.id )
+
+        if(hasFilme){
+            alert("Esse filme já está na lista")
+            return
+        }
+        filmesSalvos.push(filme)
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
+        alert("Salvo com sucesso!")
+    }
 
     useEffect(() => {
         async function loadFilme(){
@@ -56,7 +72,7 @@ export function Filme() {
             <strong>Avaliação: {filme.vote_average} / 10 </strong>
 
             <div className="area-buttons">
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
                     <a target='blank' rel='external' href={`https://youtube.com/results?search_query=${filme.title} trailer`}>
                         Trailer
